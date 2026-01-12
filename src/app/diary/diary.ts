ff096b3d69b50757fc784f23228805de94607642
Sashi Sonkusare <sonkusaresakshibhojraj@gmail.com>
2026-01-12 07:51:07 +0530

    Feature/Diary

--- src/app/diary/diary.ts
+++ src/app/diary/diary.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 @Component({
   selector: 'app-diary',
-  imports: [],
+  //standalone : true,
+  imports: [FormsModule],
   templateUrl: './diary.html',
   styleUrl: './diary.css',
 })
 export class Diary {
-
+  Title='';
+  Diary_Content='';
 }
--- src/app/menu/menu.ts
+++ src/app/menu/menu.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ToDo } from '../to-do/to-do';
 import { Diary } from '../diary/diary';
+import { Input } from '@angular/core';
+
 @Component({
   selector: 'app-menu',
   imports: [ToDo, Diary],
@@ -9,11 +11,16 @@ import { Diary } from '../diary/diary';
 })
 export class Menu {
   Message_01 = '';
-
+  Message_04 = '';
+  @Input() username!: string;
+  
+  Dashboard(){
+    this.Message_04 = `${this.username}'s Dashboard`;
+    }
   ToDo(){
     this.Message_01 = 'To-Do Component Loaded!';
 }
   Diary(){
     this.Message_01 = 'Diary Component Loaded!';
-}  
+}
 }
